052bdf62fe726e9b0e358b11afcba22538e89bd4
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2022-10-02 19:39:50 -0400

    Update and rename CheatSheet.sql to CheatSheet.mysql

--- CheatSheet.sql
+++
@@ -1,3 +0,0 @@
-// Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
-SELECT user_id,CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,length(name)))) AS name
-FROM Users ORDER BY user_id;
